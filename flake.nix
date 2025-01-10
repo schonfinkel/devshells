3af1211f9fadd30cc3182ee3668eb911af3ecba4
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-01-10 09:35:00 -0300

    nix: add F# as well

--- flake.nix
+++ flake.nix
@@ -2,8 +2,17 @@
   description = "A collection of flake templates";
 
   outputs = { self }: {
-
+    # nix flake new --template templates#<flake> ./dir
     templates = {
+      fsharp = {
+        path = ./fsharp;
+        description = "A basic web app template for F#";
+        welcomeText = ''
+          # Getting started
+          - Run `nix develop --impure`
+        '';
+      };
+
       gleam = {
         path = ./gleam;
         description = "A basic web app template for Gleam + Postgres";
