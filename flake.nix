262fbea57789da0c0e018aa0a8d692c85f5a9655
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-03-26 08:28:02 -0300

    nix: add OCaml env

--- flake.nix
+++ flake.nix
@@ -4,6 +4,15 @@
   outputs = { self }: {
     # nix flake new --template templates#<flake> ./dir
     templates = {
+      elixir = {
+        path = ./elixir;
+        description = "A basic web app template for Elixir";
+        welcomeText = ''
+          # Getting started
+          - Run `nix develop --impure`
+        '';
+      };
+
       fsharp = {
         path = ./fsharp;
         description = "A basic web app template for F#";
@@ -16,6 +25,15 @@
       gleam = {
         path = ./gleam;
         description = "A basic web app template for Gleam + Postgres";
+        welcomeText = ''
+          # Getting started
+          - Run `nix develop --impure`
+        '';
+      };
+
+      ocaml = {
+        path = ./ocaml;
+        description = "A basic web app template for OCaml";
         welcomeText = ''
           # Getting started
           - Run `nix develop --impure`
