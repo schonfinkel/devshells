7a6e5060da663faf7ea7a0db4adacec504a2fa8e
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-12-10 17:29:56 -0400

    nix: improve F# env

--- fsharp/flake.nix
+++ fsharp/flake.nix
@@ -33,19 +33,10 @@
       perSystem =
         { pkgs, system, ... }:
         let
-          dotnet =
-            with pkgs.dotnetCorePackages;
-            combinePackages [
-              sdk_10_0
-            ];
-
+          dotnet = pkgs.dotnet-sdk_10;
           linuxPkgs = with pkgs; [
             icu
             inotify-tools
-          ];
-          darwinPkgs = with pkgs.darwin.apple_sdk.frameworks; [
-            CoreFoundation
-            CoreServices
           ];
           tooling =
             with pkgs;
@@ -104,21 +95,66 @@
             # nix develop .#ci
             # reduce the number of packages to the bare minimum needed for CI
             ci = pkgs.mkShell {
-              buildInputs = dotnet ++ tooling;
+              buildInputs = tooling ++ [ dotnet ];
             };
 
             # nix develop --impure
             default = devenv.lib.mkShell {
               inherit inputs pkgs;
               modules = [
-                (import ./devshell.nix {
-                  inherit
-                    pkgs
-                    dotnet
-                    tooling
-                    app_name
-                    ;
-                })
+                (
+                  { pkgs, lib, ... }:
+                  {
+                    packages = tooling;
+
+                    languages.dotnet = {
+                      enable = true;
+                      package = dotnet;
+                    };
+
+                    scripts = {
+                      build.exec = "just build";
+                      db-up.exec = "just db-up";
+                      db-down.exec = "just db-down";
+                      db-reset.exec = "just db-reset";
+                    };
+
+                    enterShell = ''
+                      echo "Starting Development Environment..."
+                      dotnet --version
+                    '';
+
+                    services.postgres = {
+                      enable = true;
+                      package = pkgs.postgresql_17;
+                      extensions = ext: [
+                        ext.periods
+                      ];
+                      initdbArgs = [
+                        "--locale=C"
+                        "--encoding=UTF8"
+                      ];
+                      settings = {
+                        shared_preload_libraries = "pg_stat_statements";
+                        # pg_stat_statements config, nested attr sets need to be
+                        # converted to strings, otherwise postgresql.conf fails
+                        # to be generated.
+                        compute_query_id = "on";
+                        "pg_stat_statements.max" = 10000;
+                        "pg_stat_statements.track" = "all";
+                      };
+                      initialDatabases = [ { name = app_name; } ];
+                      port = 5432;
+                      listen_addresses = "127.0.0.1";
+                      initialScript = ''
+                        CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+                        CREATE USER admin SUPERUSER;
+                        ALTER USER admin PASSWORD 'admin';
+                        GRANT ALL PRIVILEGES ON DATABASE ${app_name} to admin;
+                      '';
+                    };
+                  }
+                )
               ];
             };
           };
